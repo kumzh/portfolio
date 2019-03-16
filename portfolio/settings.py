5c0f1141a3ee7aeb7eacecfae7e7cbcfbfbf081e
zhou <zk1159105388@outlook.com>
2019-03-16 14:40:48 +0800

    在配置文件里配置媒体存放位置和URL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'     #存放媒体文件
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  #文件夹路径
+
