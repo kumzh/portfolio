6c91b7fe3d158e9b3447bdd2fdeca396a37cbd75
zhou <zk1159105388@outlook.com>
2019-03-16 14:41:29 +0800

    运行

--- gallery/models.py
+++ gallery/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.    创建模型，创建之后需要注册
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default="描述",max_length=100)
+    image = models.ImageField(default="default.png",upload_to="image/")
+    title = models.CharField(default="标题",max_length=50)
+
+    def __str__(self):
+        return self.title
